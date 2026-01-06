b7719f9f5859dd5c2f5cad56a14234c19bb3ff06
ogzhnkse <okose3@gmail.com>
2026-01-06 12:18:33 +0300

    Update app.py

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ This application analyzes uploaded skin lesion photos using artificial intellige
 These results are only estimates and do not substitute for a medical diagnosis. Please consult a doctor for a definitive result.*
 """)
 
-This application analyzes uploaded skin lesion photos using artificial intelligence.
+
 
 
 
